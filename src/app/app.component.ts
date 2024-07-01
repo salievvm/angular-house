34db9daf97290c56cc83dcb2f6212557451f2bd4
salievvm <salievvm@gmail.com>
2024-07-01 10:15:09 +0500

    lesson 02: Create Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    <h1>Hello World!</h1>
-  `,
+  imports: [HomeComponent],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
