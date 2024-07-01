3775286371c04adb10f1e5b3d951458fee32f635
salievvm <salievvm@gmail.com>
2024-07-01 15:14:30 +0500

    lesson 07: use *ngFor in templates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HousingLocationComponent } from '../housing-location';
 import { HousingLocationInterface } from '../housing-location/housing-location.type';
+import { housingLocationList } from './mock/data';
 
 @Component({
   selector: 'app-home',
@@ -14,14 +15,8 @@ import { HousingLocationInterface } from '../housing-location/housing-location.t
 export class HomeComponent {
   readonly baseUrl = '/assets/houses';
 
-  housingLocation: HousingLocationInterface = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/1.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocationInterface[] = housingLocationList.map((house) => ({
+    ...house,
+    photo: `${this.baseUrl}/${house.photo}`
+  }));
 }
