c11fa7a74e112ad3342e632f6b2ac26cc886e6a5
salievvm <salievvm@gmail.com>
2024-07-01 10:30:01 +0500

    lesson 03: Create housing location component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home';
 
 @Component({
   selector: 'app-root',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HousingLocationComponent } from '../housing-location';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
