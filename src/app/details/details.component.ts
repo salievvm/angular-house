e79ba114736e7c257e376f4197d0a2f28d73225b
salievvm <salievvm@gmail.com>
2024-07-01 16:02:48 +0500

    lesson 11: Customize the details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import {
+  HousingLocationService,
+  HousingLocationInterface,
+} from '../housing-location';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  styleUrl: './details.component.scss',
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingLocationService = inject(HousingLocationService);
+  housingLocation!: HousingLocationInterface | undefined;
 
+  constructor() {
+    const housingLocationId  = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocationInterface } from './housing-location.type';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
